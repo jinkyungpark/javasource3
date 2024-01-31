d3290e0e98ab7c9b155ca5ff9118484964b50080
kyung <kyung5896@gmail.com>
2024-01-31 17:58:05 +0900

    변수, 연산자

--- ch1_variable/src/ch1/Hello.java
+++ ch1_variable/src/ch1/Hello.java
@@ -4,10 +4,11 @@ package ch1;
 // 클래스명은 한글 가능하나, 영어로 사용
 
 public class Hello {
-    public static void main(String[] args) {
-       System.out.println("안녕하세요");
-       System.out.println("안녕하세요");
-       System.out.println("안녕하세요");
-       System.out.println("안녕하세요");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("안녕하세요");
+    System.out.println("안녕하세요");
+    System.out.println("안녕하세요");
+    System.out.println("안녕하세요");
+  }
 }
